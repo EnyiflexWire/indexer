a1e4f0ad87b9ae716d9839ff7a0464bfa4c7ac9d
0xthrpw <0xthrpw@gmail.com>
2024-08-02 02:01:44 -0400

    migration fixes

--- db/schema/050__views/004/events__efp_leaderboard_mutuals/create_view__events__efp_leaderboard_mutuals.sql
+++ db/schema/050__views/004/events__efp_leaderboard_mutuals/create_view__events__efp_leaderboard_mutuals.sql
@@ -4,14 +4,14 @@
 -------------------------------------------------------------------------------
 CREATE
 OR REPLACE VIEW PUBLIC.view__events__efp_leaderboard_mutuals AS
-SELECT hexlify(r.record_data) as leader, COUNT(r.record_data) as mutuals 
+SELECT public.hexlify(r.record_data) as leader, COUNT(r.record_data) as mutuals 
 FROM public.view__join__efp_list_records_with_nft_manager_user_tags r
-WHERE hexlify(r.record_data) <> r.user
+WHERE public.hexlify(r.record_data) <> r.user
 AND EXISTS (
     SELECT 1 
     FROM public.view__join__efp_list_records_with_nft_manager_user_tags r2
-    WHERE hexlify(r2.record_data) = r.user
-    AND hexlify(r.record_data) = r2.user
+    WHERE public.hexlify(r2.record_data) = r.user
+    AND public.hexlify(r.record_data) = r2.user
 )
 GROUP BY leader;
 
