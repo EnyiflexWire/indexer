b835cb16bf7561e9ab6c4dcdfadfa954aed0455e
0xthrpw <0xthrpw@gmail.com>
2024-08-25 22:34:11 -0400

    base sep client 503

--- src/clients/viem/index.ts
+++ src/clients/viem/index.ts
@@ -69,8 +69,7 @@ export const evmClients = {
       transport: fallback(
         [
           http(`https://base-mainnet.g.alchemy.com/v2/${env.BASE_SEPOLIA_ALCHEMY_ID}`),
-          http(`https://${env.QUIKNODE_PREFIX}.base-mainnet.quiknode.pro/${env.QUIKNODE_ID}`),
-          http(`https://sepolia.base.org`)
+          http(`https://${env.QUIKNODE_PREFIX}.base-mainnet.quiknode.pro/${env.QUIKNODE_ID}`)
         ],
         { rank: true }
       ),
@@ -119,8 +118,7 @@ export const evmClients = {
       transport: fallback(
         [
           http(`https://base-sepolia.g.alchemy.com/v2/${env.BASE_SEPOLIA_ALCHEMY_ID}`),
-          http(`https://${env.QUIKNODE_PREFIX}.base-sepolia.quiknode.pro/${env.QUIKNODE_ID}`),
-          http(`https://sepolia.base.org`)
+          http(`https://${env.QUIKNODE_PREFIX}.base-sepolia.quiknode.pro/${env.QUIKNODE_ID}`)
         ],
         { rank: true }
       ),
